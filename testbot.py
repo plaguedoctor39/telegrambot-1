41a2325ed04c368d9abac8ac49007cb9bfd7be0d
daniil <shishovdanil@gmail.com>
2020-02-18 16:45:25 +0300

    added reply to msg

--- testbot.py
+++ testbot.py
@@ -6,7 +6,10 @@ bot = telebot.TeleBot(config.BOT_TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def response(message):
-    bot.send_message(message.chat.id, message.text)
+    if message.text.lower() == 'durak' :
+    bot.reply_to(message, 'Sam {!s}'.format(message.text))
+    else:
+        bot.send_message(message.chat.id, message.text)
 
 
 # Start
